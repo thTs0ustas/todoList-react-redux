2b9acfcb99251a38a732c8db93cef091e84eeb8e
thTs0ustas <th.ts0ustas@gmail.com>
2020-04-13 18:02:32 +0300

    now-can-use-redux-toolkit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { todoApp } from "./redux/reducers/conbinedReducers";
 import { Provider } from "react-redux";
 
-const store = createStore(todoApp);
+const store = configureStore({
+  reducer: todoApp,
+});
 
 ReactDOM.render(
   <Provider store={store}>
